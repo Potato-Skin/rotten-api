4203f4163e46ba1495815975e007446f9110fa7e
André de Albuquerque <itstheandre@gmail.com>
2021-05-27 21:43:48 +0200

    CSS

--- models/Rating.model.js
+++ models/Rating.model.js
@@ -1,6 +1,16 @@
 const { Schema, model } = require("mongoose");
 
-const ratingSchema = new Schema({});
+const ratingSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  rating: {
+    required: true,
+    type: Number,
+  },
+});
 
 const Rating = model("Rating", ratingSchema);
 
--- routes/movies.js
+++ routes/movies.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const isLoggedIn = require("../middleware/isLoggedIn");
 const Movie = require("../models/Movie.model");
 const Review = require("../models/Review.model");
+const Rating = require("../models/Rating.model");
 
 const router = Router();
 
@@ -12,14 +13,22 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:allTheSingleMovies", (req, res) => {
+  // http://localhost:5000/api/movies/laura
+  // http://localhost:5000/api/movies/khryis
+  // http://localhost:5000/api/movies/luis
+  // {allTheSingleMovies: laura}
+  // {allTheSingleMovies: khryis}
   Movie.findById(req.params.allTheSingleMovies)
     .populate("reviews")
+    .populate("ratings")
     .then((movie) => {
       res.json(movie);
     });
 });
 
 router.post("/add", isLoggedIn, (req, res) => {
+  // http://localhost:5000/api/movies/add
+  // {}
   Movie.findOne({
     title: req.body.title,
   })
@@ -78,6 +87,47 @@ router.post("/:id/add-review", isLoggedIn, (req, res) => {
         res.json({ message: "Cool beans, brah", movie: updatedMovie });
       });
   });
+});
+
+router.post("/:movieId/add-rating", isLoggedIn, (req, res) => {
+  const { rating } = req.body;
+
+  if (!rating) {
+    // 4xx CLIENT ERROR || 5xx SERVER ERROR
+    return res.status(400).json({
+      errMessage:
+        "Thats no rating, i don't know. whatever.... This is not the André you're looking for",
+    });
+  }
+
+  Rating.create({
+    rating,
+    user: req.user._id,
+  })
+    .then((newRating) => {
+      console.log("newRating:", newRating);
+
+      Movie.findByIdAndUpdate(
+        req.params.movieId,
+        { $addToSet: { ratings: newRating._id } },
+        { new: true }
+      )
+
+        .populate("ratings")
+        .populate("reviews")
+
+        .then((updatedMovie) => {
+          res.json({
+            message: "These are indeed the André you're looking for",
+            movie: updatedMovie,
+          });
+        });
+    })
+
+    .catch((err) => {
+      console.error(err.message);
+      res.status(500).json({ errorMessage: "I don't know. Do you?" });
+    });
 });
 
 module.exports = router;
